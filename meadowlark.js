73a57f2ed9a7c04b2cecdd035f47b8f24a7611db
Arjun Lubana <arjunlubana562@gmail.com>
2022-01-04 03:09:59 +0300

    Adding a weather widget partials

--- meadowlark.js
+++ meadowlark.js
@@ -1,40 +1,62 @@
-import express from "express";
-import { engine } from "express-handlebars";
-import { dirname } from "path";
-import getFortune from "./lib/fortune.js";
+var express = require("express");
+var engine = require("express-handlebars").engine;
+var dirname = require("path").dirname;
+var getFortune = require("./lib/fortune.js");
+var getWeatherData = require("./lib/weather.js");
 
-
-const app = express();
+var app = express();
 
 app.set("port", process.env.PORT || 3000);
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
-app.use(express.static(dirname(".") + "/public"));
 
-app.get("/", (req, res) => {
+// Middleware
+
+app.use(express.static(dirname(".") + "/public"));
+app.use(function (req, res, next) {
+  res.locals.showTests =
+    app.get("env") !== "production" && req.query.test === "1";
+  next();
+});
+app.use(function (req, res, next) {
+  if (!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.weather = getWeatherData();
+  next();
+});
+
+app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", {fortune: getFortune()});
+app.get("/about", function (req, res) {
+  res.render("about", {
+    fortune: getFortune(),
+    pageTestScript: "/qa/tests-about.js",
+  });
 });
 
+app.get("/tours/hood-river", function (req, res) {
+  res.render("tours/hood-river");
+});
+app.get("/tours/request-group-rate", function (req, res) {
+  res.render("tours/request-group-rate");
+});
 // Custom 404 Page
 
-app.use((req, res) => {
+app.use(function (req, res) {
   res.status(404);
   res.render("404");
 });
 
 // Custom 500 Page
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
   console.error(err.stack);
   res.status(500);
   res.render("500");
 });
 
-app.listen(app.get("port"), () => {
+app.listen(app.get("port"), function () {
   console.log(
     "Express started on http://localhost:" + app.get("port"),
     "\nPress Ctrl-C to terminate."
